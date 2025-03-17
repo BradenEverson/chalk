f121c4b83b0d47cc9eb030193bbcb63373c3d2ca
Braden Everson <bradeneverson@gmail.com>
2025-03-17 15:15:14 -0500

    Begin creating AST parser

--- chalk-core/src/ast.rs
+++ chalk-core/src/ast.rs
@@ -1,1 +1,103 @@
 //! Abstract Syntax Tree data structures and evaluation methods
+
+use std::fmt::Display;
+
+use crate::tokenizer::Token;
+
+/// A node in the AST
+#[derive(Clone, Debug, PartialEq)]
+pub enum Expr {
+    /// Number leaf node (integer)
+    Integer(i32),
+    /// Number leaf node (real)
+    Real(f32),
+    /// Binary operator node
+    BinaryOp {
+        /// The operation
+        op: BinaryOperator,
+        /// Left edge
+        left: Box<Expr>,
+        /// Right edge
+        right: Box<Expr>,
+    },
+    /// Parenthesis around an expr
+    Paren(Box<Expr>),
+}
+
+/// All binary operations
+#[derive(Clone, Debug, PartialEq)]
+pub enum BinaryOperator {
+    /// Adding
+    Add,
+    /// Subtracting
+    Subtract,
+    /// Multiplying
+    Multiply,
+    /// Dividing
+    Divide,
+}
+
+/// A parser object for wrapping over a token span and keeping track of index during parsing
+#[derive(Clone, Debug, PartialEq)]
+pub struct Parser {
+    tokens: Vec<Token>,
+    current: usize,
+}
+
+/// Generic parser error
+#[derive(Debug)]
+pub struct ParseError;
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Parse error occurred :(")
+    }
+}
+
+impl Parser {
+    /// Creates a new parser from a token span
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Parser { tokens, current: 0 }
+    }
+
+    /// Parses the current token span into an AST
+    pub fn parse(&mut self) -> Result<Expr, ParseError> {
+        todo!("Parse")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::tokenizer::Tokenizable;
+
+    use super::*;
+
+    #[test]
+    fn test_ast() {
+        let tokens = "(1 + 2) * 3 - 4 / 2"
+            .tokenize()
+            .expect("Tokenization failed");
+        let mut parser = Parser::new(tokens);
+        let ast = parser.parse().expect("Parsing failed");
+
+        let expected = Expr::BinaryOp {
+            op: BinaryOperator::Subtract,
+            left: Box::new(Expr::BinaryOp {
+                op: BinaryOperator::Multiply,
+                left: Box::new(Expr::Paren(Box::new(Expr::BinaryOp {
+                    op: BinaryOperator::Add,
+                    left: Box::new(Expr::Integer(1)),
+                    right: Box::new(Expr::Integer(2)),
+                }))),
+                right: Box::new(Expr::Integer(3)),
+            }),
+            right: Box::new(Expr::BinaryOp {
+                op: BinaryOperator::Divide,
+                left: Box::new(Expr::Integer(4)),
+                right: Box::new(Expr::Integer(2)),
+            }),
+        };
+
+        assert_eq!(ast, expected);
+    }
+}
