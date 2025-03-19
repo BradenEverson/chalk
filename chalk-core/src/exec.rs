4c7c002771681760ec6d45867597720a66d1ba14
Braden Everson <bradeneverson@gmail.com>
2025-03-19 08:07:33 -0500

    Create result type

--- chalk-core/src/exec.rs
+++ chalk-core/src/exec.rs
@@ -1,9 +1,72 @@
 //! AST Execution/Evaluation
 
+use std::{error::Error, fmt::Display};
+
 use crate::{
     ast::{BinaryOperator, Expr, UnaryOperator},
     math::{gcd::gcd, lcm::lcm},
 };
+
+/// A runtime type error
+#[derive(Debug, Clone, Copy)]
+pub struct RuntimeError;
+
+impl Display for RuntimeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Runtime error occurred :( your types are probably not good"
+        )
+    }
+}
+
+impl Error for RuntimeError {}
+
+/// All results an AST may have
+pub enum EvalResult {
+    /// An integer
+    Integer(i32),
+    /// A float
+    Float(f32),
+    /// A bool
+    Bool(bool),
+}
+
+impl EvalResult {
+    /// Gets the result assuming it to be an int, asserting it so through a runtime error
+    pub fn int(&self) -> Result<i32, RuntimeError> {
+        match self {
+            Self::Integer(i) => Ok(*i),
+            _ => Err(RuntimeError),
+        }
+    }
+
+    /// Gets the result assuming it to be an int, asserting it so through a runtime error
+    pub fn float(&self) -> Result<f32, RuntimeError> {
+        match self {
+            Self::Float(f) => Ok(*f),
+            _ => Err(RuntimeError),
+        }
+    }
+
+    /// Gets the result assuming it to be a bool, asserting it so through a runtime error
+    pub fn bool(&self) -> Result<bool, RuntimeError> {
+        match self {
+            Self::Bool(b) => Ok(*b),
+            _ => Err(RuntimeError),
+        }
+    }
+}
+
+impl Display for EvalResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Bool(b) => write!(f, "{b}"),
+            Self::Integer(i) => write!(f, "{i}"),
+            Self::Float(fl) => write!(f, "{fl}"),
+        }
+    }
+}
 
 impl UnaryOperator {
     /// Evaluates a left and right value with relation to the current operation
